de6f10754e1507d4bee94bbc69cc19feb42cf3e9
Mohit-Nathrani-at-appointy <127097824+Mohit-Nathrani-at-appointy@users.noreply.github.com>
2023-08-05 05:34:19 +0530

    Update structs.go

--- structs.go
+++ structs.go
@@ -1,13 +1,13 @@
 package main
-​
-import "time"
-​
+
+// entities
+
 type Resource struct {
 	Id          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
-​
+
 type BusinessHour struct {
 	Id         string    `json:"id"`
 	ResourceId string    `json:"resource_id"`
@@ -15,14 +15,14 @@ type BusinessHour struct {
 	StartTime  string    `json:"start_time"`
 	EndTime    string    `json:"end_time"`
 }
-​
+
 type BlockHour struct {
 	Id         string    `json:"id"`
 	ResourceId string    `json:"resource_id"`
 	StartTime  string    `json:"start_time"`
 	EndTime    string    `json:"end_time"`
 }
-​
+
 type Appointment struct {
 	Id         string    `json:"id"`
 	ResourceId string    `json:"resource_id"`
@@ -30,7 +30,27 @@ type Appointment struct {
 	StartTime  string    `json:"start_time"`
 	EndTime    string    `json:"end_time"`
 }
-​
+
 type Duration struct {
 	Seconds int64 `json:"seconds"`
+}
+
+// endpoint request structs
+
+type ListBusinessHoursRequest struct {
+	ResourceId string `json:"resourceId"`
+	StartTime  string `json:"startTime"`
+	EndTime    string `json:"endTime"`
+}
+
+type ListBlockHoursRequest struct {
+	ResourceId string `json:"resourceId"`
+	StartTime  string `json:"startTime"`
+	EndTime    string `json:"endTime"`
+}
+
+type ListAppointmentRequest struct {
+	ResourceId string `json:"resourceId"`
+	StartTime  string `json:"startTime"`
+	EndTime    string `json:"endTime"`
 }
